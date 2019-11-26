1b128db6dc25c61e85779253ea0fe46ad6c18eb4
John <flippy2016@gmail.com>
2019-11-26 22:01:24 +0700

    Paging list of questions, handle submit quiz button

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
+import {
+  faClock,
+  faFile, faPen, faPaperPlane, faAward,
+  faAngleDoubleLeft, faAngleLeft, faAngleRight, faAngleDoubleRight
+} from '@fortawesome/free-solid-svg-icons';
 import Swal from 'sweetalert2';
 import { IQuiz } from 'src/app/models/IQuiz';
+import { IPager } from 'src/app/models/IPager';
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
@@ -14,26 +19,49 @@ export class QuizComponent implements OnInit {
   numberAnswers = 0;
   isSubmit = false;
 
-  timeLeft: number;
+  maxtime: number;
+  currentTime: number;
   interval;
-  ellapsedTime = '00:00';
+  ellapsedMaxTime = '00:00';
+  ellapsedCurrentTime = '00:00';
   isActiveStart: boolean;
   isActivePause: boolean;
   isActiveReset: boolean;
+
+  // Font-awesome icon
   faClock = faClock;
+  faPen = faPen;
+  faFile = faFile;
+  faPaperPlane = faPaperPlane;
+  faAward = faAward;
+  faAngleDoubleLeft = faAngleDoubleLeft;
+  faAngleLeft = faAngleLeft;
+  faAngleRight = faAngleRight;
+  faAngleDoubleRight = faAngleDoubleRight;
+
+  // Page
+  isOpenQuizPage: boolean;
+  pager: IPager;
 
   constructor() { }
 
   ngOnInit() {
-    this.timeLeft = 60;
-    this.ellapsedTime = this.parseTime(this.timeLeft);
+    this.currentTime = 0;
+    this.maxtime = 60;
+    this.isOpenQuizPage = true;
+    this.ellapsedMaxTime = this.parseTime(this.maxtime);
+    this.ellapsedCurrentTime = this.parseTime(this.currentTime);
     this.isActiveStart = true;
     this.isActivePause = false;
     this.isActiveReset = false;
-
+    this.pager = {
+      index: 0,
+      size: 1,
+      count: 1
+    };
     this.quiz = {
       id: 1,
-      name: 'Ôn tập toán giữa học kỳ 1',
+      name: 'Ôn tập giữa học kỳ 1',
       description: 'this is my quiz',
       questions: [
         {
@@ -145,28 +173,167 @@ export class QuizComponent implements OnInit {
           questionTypeId: 2,
           correctAnswer: '4',
           userAnswer: ''
+        },
+        {
+          id: 11,
+          name: '3 + 2 ... 5',
+          questionTypeId: 1,
+          options: [
+            {
+              id: 8,
+              questionId: 11,
+              name: '>',
+              isAnswer: false
+            },
+            {
+              id: 9,
+              questionId: 11,
+              name: '=',
+              isAnswer: true
+            },
+            {
+              id: 10,
+              questionId: 11,
+              name: '<',
+              isAnswer: false
+            }]
+        },
+        {
+          id: 12,
+          name: 'Số lớn nhất trong các số: 2, 5, 7, 9 là:',
+          questionTypeId: 1,
+          options: [
+            {
+              id: 11,
+              questionId: 12,
+              name: '2',
+              isAnswer: false
+            },
+            {
+              id: 12,
+              questionId: 12,
+              name: '5',
+              isAnswer: false
+            },
+            {
+              id: 13,
+              questionId: 12,
+              name: '7',
+              isAnswer: false
+            },
+            {
+              id: 14,
+              questionId: 12,
+              name: '9',
+              isAnswer: true
+            }]
+        },
+        {
+          id: 13,
+          name: 'Số lớn nhất trong các số: 7, 4, 8, 6 là:',
+          questionTypeId: 1,
+          options: [
+            {
+              id: 15,
+              questionId: 13,
+              name: '7',
+              isAnswer: false
+            },
+            {
+              id: 16,
+              questionId: 13,
+              name: '4',
+              isAnswer: false
+            },
+            {
+              id: 17,
+              questionId: 13,
+              name: '6',
+              isAnswer: false
+            },
+            {
+              id: 18,
+              questionId: 13,
+              name: '8',
+              isAnswer: true
+            }]
+        },
+        {
+          id: 14,
+          name: 'Dãy các số theo thứ tự từ 0 đến 10',
+          questionTypeId: 1,
+          options: [
+            {
+              id: 19,
+              questionId: 14,
+              name: '0, 1, 2, 3, 4, 5, 6, 7, 8 ,9, 10',
+              isAnswer: true
+            },
+            {
+              id: 20,
+              questionId: 14,
+              name: '0, 1, 2, 3, 4, 7, 6, 5, 8, 9, 10',
+              isAnswer: false
+            },
+            {
+              id: 21,
+              questionId: 14,
+              name: '0, 2, 1, 3, 4, 5, 6, 7, 8, 9, 10',
+              isAnswer: false
+            },
+            {
+              id: 22,
+              questionId: 14,
+              name: '0, 1, 2, 7, 4, 5, 6, 8, 3, 9, 10',
+              isAnswer: false
+            }]
+        },
+        {
+            id: 15,
+            name: '7 + 1 = ...',
+            questionTypeId: 2,
+            correctAnswer: '8',
+            userAnswer: ''
         }]
     };
 
     this.shuffle(this.quiz.questions);
     this.numberAnswers = this.quiz.questions.length;
+    this.pager.count = this.numberAnswers;
     this.start();
   }
 
+  // Handle event when user clicked button Nop bai
   onSubmit() {
+    this.pause();
+    if (this.currentTime < this.maxtime) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Thông báo',
+        text: 'Bạn có nộp bài hay không ?',
+        showCancelButton: true,
+        confirmButtonText: 'Có',
+        cancelButtonText: 'Không',
+        allowOutsideClick: false
+      }).then((result) => {
+        if (result.value) {
+          this.submitQuiz();
+        } else {
+          this.start();
+        }
+      });
+    }
+
+  }
+
+  // Submit quiz
+  submitQuiz() {
     this.isSubmit = true;
+    this.isOpenQuizPage = false;
     this.isActivePause = false;
     this.isActiveReset = true;
+    clearInterval(this.interval);
     this.scoreCalculation();
-    
-    let submitAlertText = `Số lượng câu trả lời đúng của bạn: ${this.correctAnswers} / ${this.numberAnswers} câu`
-
-    Swal.fire({
-      icon: 'info',
-      title: 'Thông báo',
-      text: submitAlertText,
-      allowOutsideClick: false
-    });
   }
 
   scoreCalculation() {
@@ -198,23 +365,24 @@ export class QuizComponent implements OnInit {
     this.isActivePause = true;
 
     this.interval = setInterval(() => {
-      if (this.timeLeft === 0) {
+      if (this.currentTime === this.maxtime) {
         clearInterval(this.interval);
         Swal.fire({
           icon: 'info',
           title: 'Hết giờ!',
           text: 'Đã hết thời gian làm bài!',
-          allowOutsideClick: false
+          allowOutsideClick: false,
+          confirmButtonText: 'Xem kết quả'
         }).then((result) => {
           if (result.value) {
-            this.onSubmit();
+            this.submitQuiz();
           }
         });
       }
 
-      if (this.timeLeft > 0) {
-        this.timeLeft--;
-        this.ellapsedTime = this.parseTime(this.timeLeft);
+      if (this.currentTime < this.maxtime) {
+        this.currentTime++;
+        this.ellapsedCurrentTime = this.parseTime(this.currentTime);
       } else {
       }
     }, 1000);
@@ -248,14 +416,41 @@ export class QuizComponent implements OnInit {
     this.start();
   }
 
+  // Shuffle questions on quiz
   shuffle(listQuestions) {
     for (let i = listQuestions.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      let x = listQuestions[i];
+      const j = Math.floor(Math.random() * (i + 1));
+      const x = listQuestions[i];
       listQuestions[i] = listQuestions[j];
       listQuestions[j] = x;
     }
 
     return listQuestions;
+  }
+
+  get filteredQuestions() {
+    return (this.quiz.questions) ? this.quiz.questions.slice(this.pager.index, this.pager.index + this.pager.size) : [];
+  }
+
+  get isFirstQuestion() {
+    return this.pager.index === 0;
+  }
+
+  get checkNextQuestion() {
+    return this.pager.index + 1 > this.quiz.questions.length - 1;
+  }
+
+  get checkPreviousQuestion() {
+    return this.pager.index - 1 < 0;
+  }
+
+  get isLastQuestion() {
+    return this.pager.index === this.quiz.questions.length - 1;
+  }
+
+  goTo(index: number) {
+    if (index >= 0 && index < this.pager.count) {
+      this.pager.index = index;
+    }
   }
 }
--- src/app/models/IQuiz.ts
+++ src/app/models/IQuiz.ts
@@ -1,8 +1,8 @@
 import { IQuestion } from './IQuestion';
 
 export interface IQuiz {
-    id: number;
-    name: string;
-    description: string;
-    questions: IQuestion[];
-  }
+  id: number;
+  name: string;
+  description: string;
+  questions: IQuestion[];
+}
